381674cf1d953b5add5034ae8922836ea134ae13
Janna Nugent <janna.nugent@northwestern.edu>
2016-10-18 21:34:07 -0500

    removes fasta entries that are too short

---
+++ trim_fasta.py
@@ -0,0 +1,30 @@
+# accepts as input filename and quality cutoff score
+import sys
+from Bio import SeqIO
+
+filename = sys.argv[1]
+if (filename[-1] != 'a'):
+    print("please enter a fasta file as the first argument.")
+    sys.exit()
+file_prefix = filename.split('.')[0]
+output_file = file_prefix + '.trimmed.fasta'
+
+read_threshold = int(sys.argv[2])
+#    sys.exit() 
+
+print("filename = ", filename, "read length = ", read_threshold)
+
+print('')
+handle = open(filename, "rU")
+output = open(output_file, "w")
+for record in SeqIO.parse(handle, "fasta"):
+    print(record.id)
+    
+    if len(str(record.seq)) > read_threshold:
+         output.write(record.id)
+         output.write('\n')
+         output.write(str(record.seq))
+         output.write('\n')
+    
+handle.close()
+output.close()
