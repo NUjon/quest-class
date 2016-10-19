3e483ee585a250d551c77480aab4eee7501eecbe
Janna Nugent <janna.nugent@northwestern.edu>
2016-10-18 21:17:39 -0500

    added inputs and output to fasta file

--- test.py
+++ test.py
@@ -1,23 +1,37 @@
+# accepts as input filename and quality cutoff score
+import sys
 from Bio import SeqIO
 
-#handle = open("SRR062634_1.filt.fastq", "rU")
+filename = sys.argv[1]
+if (filename[-1] != 'q'):
+    print("please enter a fastq file as the first argument.")
+    sys.exit()
+file_prefix = filename.split('.')[0]
+output_file = file_prefix + '.fasta'
+
+quality_level = int(sys.argv[2])
+if  quality_level > 60:
+    print("please enter a quality level as the second argument between 1 - 60.")
+    sys.exit() 
+
+print("filename = ", filename, "quality level = ", quality_level)
+
 print('')
-handle = open("exome.fastq", "rU")
+handle = open(filename, "rU")
+output = open(output_file, "w")
 for record in SeqIO.parse(handle, "fastq"):
     qual = record.format("qual")
     qual_list = qual.split()
     del qual_list[0]
     del qual_list[0]
-    print(qual_list)
+#    print(qual_list)
     longest_read, bp_length, start_idx, end_idx, best_start, best_end = 0, 0, 0, 0, 0, 0 
 
     for index, score in enumerate(qual_list):
         bp_length = bp_length + 1
-        if int(score) < 30:
+        if int(score) < quality_level:
             if bp_length > longest_read:
                 longest_read = bp_length 
-       #         end_idx = index
-       #         best_end = end_idx
                 best_end = index
                 best_start = start_idx
             start_idx = index+1
@@ -28,7 +42,14 @@ for record in SeqIO.parse(handle, "fastq"):
         best_start = start_idx
     print('the longest read for this record is', longest_read)
     print('it starts at', best_start, ' and ends at ', best_end)
+
+    output.write(record.id)
+    output.write('\n')
+    output.write(str(record.seq[best_start:best_end]))
+    output.write('\n')
+    
     print("length of qual_list = ", len(qual_list))
-#    print(record.seq)
+    
     print('')
 handle.close()
+output.close()
